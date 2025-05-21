4af57510771b7a4c623b38d76dcdc5053a288c2b
Ryan Zigler <rpzigler@gmail.com>
2025-05-21 14:05:27 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 pip install -U "huggingface_hub[cli]"
+wait $(jobs -p)
 git config --global credential.helper store
 huggingface-cli login --token $HF_CLI_TOKEN --add-to-git-credential
-huggingface-cli download m3gan massedcompute.sh
+huggingface-cli download godzigga/m3gan massedcompute.sh
 chmod +x massedcompute.sh
 ./massedcompute.sh
